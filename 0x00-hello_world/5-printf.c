93496f9bde9d0e99896bed93b978a6538e22670b
Mohammad <muhdsuleh1@gmail.com>
2022-06-03 08:50:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,37 +1,10 @@
-/*
- *
- *  * File: 5-printf.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
-
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Prints "with proper grammar, but the outcome is
- *
- *   *                a piece of art," followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * main - mani block
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
-
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
