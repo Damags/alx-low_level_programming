11a1fa5b6d7a68a62262331a46abf0e6a1ecb69b
Mohammad <muhdsuleh1@gmail.com>
2022-06-03 10:27:54 -0700

    mine

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 /**
- *main - Entry point
- * Return: 1
+ * main - Entry point
+ * Return: Always 1
+ *
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
