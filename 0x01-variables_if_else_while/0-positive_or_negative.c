ff286bb80f5c44c0584e4a07ddffb2322974f1bc
Mohammad <muhdsuleh1@gmail.com>
2022-06-05 02:26:18 -0700

    mine

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 /**
  * main - main block
  * Description: Get a random number and print the number
+ * This is another line of description
  * and if it is positive, negative, or zero
  * Return 0
  */
